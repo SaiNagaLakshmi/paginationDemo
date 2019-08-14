8a6ee42e6fb16844dd7e29fb607cdd4981221699
anagalakshmi.ip <anagalakshmi.ip@gmail.com>
2019-08-14 03:45:54 -0700

    paginationDemo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import {Switch,Route} from 'react-router-dom';
+import Home from './component/home';
+import Postlist from './component/postlist';
+import Practice from './component/practice';
+import All from './component/all';
+import Sidemenu from './component/sidemenu';
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+       <Switch>
+       <Route exact path='/home' component={Home} />
+       <Route path='/postlist' component={Postlist} />
+       <Route path='/practice' component={Practice} />
+       <Route path='/all' component={All} />
+       <Route path='/sidemenu' component={Sidemenu} />
+       </Switch>
+     
+      </div>
+    );
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  }
+  
 }
 
 export default App;
